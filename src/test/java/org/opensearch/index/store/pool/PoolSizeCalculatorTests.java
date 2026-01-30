42bc0f87c3c09764fcae7b2416ee519e82c17732
Shubham Kumar <shubhmkr@amazon.com>
2026-01-30 04:47:13 +0000

    Apply code formatting with spotless
    Signed-off-by: Shubham Kumar <shubhmkr@amazon.com>

--- src/test/java/org/opensearch/index/store/pool/PoolSizeCalculatorTests.java
+++ src/test/java/org/opensearch/index/store/pool/PoolSizeCalculatorTests.java
@@ -4,36 +4,31 @@
  */
 package org.opensearch.index.store.pool;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
+
 import java.lang.management.ManagementFactory;
 import java.lang.reflect.Method;
-import java.util.Arrays;
 import java.util.List;
 
 import org.opensearch.common.settings.Settings;
 import org.opensearch.test.OpenSearchTestCase;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.lessThanOrEqualTo;
-
 public class PoolSizeCalculatorTests extends OpenSearchTestCase {
 
     public void testCalculatePoolSizeWithDirectMemory() {
-        Settings settings = Settings.builder()
-            .put(PoolSizeCalculator.NODE_POOL_SIZE_PERCENTAGE_SETTING.getKey(), 0.3)
-            .build();
+        Settings settings = Settings.builder().put(PoolSizeCalculator.NODE_POOL_SIZE_PERCENTAGE_SETTING.getKey(), 0.3).build();
 
         long poolSize = PoolSizeCalculator.calculatePoolSize(settings);
-        
+
         // Pool size should be positive and reasonable
         assertThat(poolSize, greaterThan(0L));
         assertThat(poolSize, greaterThan(256L * 1024L * 1024L)); // At least 256MB minimum
     }
 
     public void testCalculatePoolSizeWithCustomPercentage() {
-        Settings settings = Settings.builder()
-            .put(PoolSizeCalculator.NODE_POOL_SIZE_PERCENTAGE_SETTING.getKey(), 0.5)
-            .build();
+        Settings settings = Settings.builder().put(PoolSizeCalculator.NODE_POOL_SIZE_PERCENTAGE_SETTING.getKey(), 0.5).build();
 
         long poolSize = PoolSizeCalculator.calculatePoolSize(settings);
         assertThat(poolSize, greaterThan(0L));
@@ -53,7 +48,7 @@ public class PoolSizeCalculatorTests extends OpenSearchTestCase {
         assertThat((Long) parseMemorySize.invoke(null, "1g"), equalTo(1024L * 1024L * 1024L));
         assertThat((Long) parseMemorySize.invoke(null, "1G"), equalTo(1024L * 1024L * 1024L));
         assertThat((Long) parseMemorySize.invoke(null, "512m"), equalTo(512L * 1024L * 1024L));
-        
+
         // Test invalid formats
         assertThat((Long) parseMemorySize.invoke(null, "invalid"), equalTo(0L));
         assertThat((Long) parseMemorySize.invoke(null, ""), equalTo(0L));
@@ -66,10 +61,10 @@ public class PoolSizeCalculatorTests extends OpenSearchTestCase {
         getMaxDirectMemory.setAccessible(true);
 
         long maxDirectMemory = (Long) getMaxDirectMemory.invoke(null);
-        
+
         // Should return a positive value (either from JVM args or default to heap size)
         assertThat(maxDirectMemory, greaterThan(0L));
-        
+
         // Should not exceed total heap size by too much (reasonable upper bound)
         long maxHeap = Runtime.getRuntime().maxMemory();
         assertThat(maxDirectMemory, lessThanOrEqualTo(maxHeap * 2)); // Allow up to 2x heap as reasonable upper bound
@@ -77,20 +72,17 @@ public class PoolSizeCalculatorTests extends OpenSearchTestCase {
 
     public void testDirectMemoryAccountingInPoolCalculation() throws Exception {
         // Test that direct memory is properly subtracted from available memory
-        Settings settings = Settings.builder()
-            .put(PoolSizeCalculator.NODE_POOL_SIZE_PERCENTAGE_SETTING.getKey(), 0.3)
-            .build();
+        Settings settings = Settings.builder().put(PoolSizeCalculator.NODE_POOL_SIZE_PERCENTAGE_SETTING.getKey(), 0.3).build();
 
         // Get the current JVM arguments to verify direct memory is being read
         List<String> jvmArgs = ManagementFactory.getRuntimeMXBean().getInputArguments();
-        boolean hasDirectMemoryArg = jvmArgs.stream()
-            .anyMatch(arg -> arg.startsWith("-XX:MaxDirectMemorySize="));
+        boolean hasDirectMemoryArg = jvmArgs.stream().anyMatch(arg -> arg.startsWith("-XX:MaxDirectMemorySize="));
 
         long poolSize = PoolSizeCalculator.calculatePoolSize(settings);
-        
+
         // Pool size should be calculated correctly regardless of whether direct memory is explicitly set
         assertThat(poolSize, greaterThan(0L));
-        
+
         // If direct memory is set, verify it's being accounted for
         if (hasDirectMemoryArg) {
             // The pool size should be less than what it would be without direct memory accounting
@@ -112,9 +104,7 @@ public class PoolSizeCalculatorTests extends OpenSearchTestCase {
         assertThat(smallRatio, equalTo(0.5)); // Reduced ratio for small instances
 
         // Test custom setting
-        Settings customSettings = Settings.builder()
-            .put(PoolSizeCalculator.NODE_CACHE_TO_POOL_RATIO_SETTING.getKey(), 0.8)
-            .build();
+        Settings customSettings = Settings.builder().put(PoolSizeCalculator.NODE_CACHE_TO_POOL_RATIO_SETTING.getKey(), 0.8).build();
         double customRatio = PoolSizeCalculator.calculateCacheToPoolRatio(offHeapBytes, customSettings);
         assertThat(customRatio, equalTo(0.8));
     }
@@ -132,22 +122,21 @@ public class PoolSizeCalculatorTests extends OpenSearchTestCase {
         assertThat(smallWarmup, equalTo(0.0)); // No warmup for small instances
 
         // Test custom setting
-        Settings customSettings = Settings.builder()
-            .put(PoolSizeCalculator.NODE_WARMUP_PERCENTAGE_SETTING.getKey(), 0.1)
-            .build();
+        Settings customSettings = Settings.builder().put(PoolSizeCalculator.NODE_WARMUP_PERCENTAGE_SETTING.getKey(), 0.1).build();
         double customWarmup = PoolSizeCalculator.calculateWarmupPercentage(offHeapBytes, customSettings);
         assertThat(customWarmup, equalTo(0.1));
     }
 
     public void testMinimumPoolSizeEnforced() {
         // Test with very small percentage to ensure minimum is enforced
-        Settings settings = Settings.builder()
+        Settings settings = Settings
+            .builder()
             .put(PoolSizeCalculator.NODE_POOL_SIZE_PERCENTAGE_SETTING.getKey(), 0.001) // Very small percentage
             .build();
 
         long poolSize = PoolSizeCalculator.calculatePoolSize(settings);
         long minExpected = 256L * 1024L * 1024L; // 256 MB minimum
-        
+
         assertThat("Pool size should enforce minimum of 256MB", poolSize, greaterThan(minExpected - 1));
     }
 }
