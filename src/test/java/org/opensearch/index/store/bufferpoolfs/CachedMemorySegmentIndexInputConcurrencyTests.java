b55861589017cd3ebe3c63e54f121d5a59c78565
Prudhvi Godithi <pgodithi@amazon.com>
2026-04-02 14:43:37 -0700

    Add tests
    Signed-off-by: Prudhvi Godithi <pgodithi@amazon.com>

--- src/test/java/org/opensearch/index/store/bufferpoolfs/CachedMemorySegmentIndexInputConcurrencyTests.java
+++ src/test/java/org/opensearch/index/store/bufferpoolfs/CachedMemorySegmentIndexInputConcurrencyTests.java
@@ -4,7 +4,6 @@
  */
 package org.opensearch.index.store.bufferpoolfs;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
